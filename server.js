7d0c9c12120dc794bc26a7ae7a237233e45661cf
katsumata <s.katsumata.0507@gmail.com>
2023-09-21 12:07:29 +0900

    repaired

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
-var path = require('path');
+const path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/dist/"));
+app.route('/*').get(function(req, res) { 
+    return res.sendFile(path.join(__dirname, 'dist/index.html')); 
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
